dd7275c3a94ec86326c073baaf64e180ae22889d
Ibrahim <ibrahimik01071@gmail.com>
2025-04-18 17:00:01 +0530

    made changes in css , added tree model etc

--- src/character.js
+++ src/character.js
@@ -1,37 +1,71 @@
-import * as THREE from 'three';
-import { FBXLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { FBXLoader, OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { AxesHelper } from "three";
 
-console.log("character.js")
-
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('threeView');
-  console.log(container)
-  if (!container) {
-    return
-  }
+document.addEventListener("DOMContentLoaded", () => {
+ 
+  const container = document.getElementById("threeView");
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffffff);
-  const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-  camera.position.z = -.4;
-  camera.position.y = .3;
-  camera.position.x = .2;
-  camera.lookAt(0, 0, 0);
+  scene.background = new THREE.Color(0x87ceeb);
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    container.clientWidth / container.clientHeight,
+    0.01,
+    10000
+  );
+  camera.position.z = -.5;
+  camera.position.y = .08;
+  camera.position.x = 0.72;
+  camera.lookAt(0,0.2,0)
 
-  // Add ambient light
+
+ const handleResize = () => {
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+  
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  
+ 
+};
+
+window.addEventListener("resize", handleResize);
+
+
   const ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
   scene.add(ambientLight);
 
-
-  // Add directional light
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+ 
+  const directionalLight = new THREE.DirectionalLight(0xffffff,5);
   directionalLight.position.set(5, 10, 0);
   scene.add(directionalLight);
+  directionalLight.castShadow = true;
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.shadowMap.enabled = true; 
   container.appendChild(renderer.domElement);
+
+
+  const control = new OrbitControls(camera, renderer.domElement);
+  control.enableDamping = true;
+  control.target= new THREE.Vector3(0,0.2,0)
+  control.autoRotate = true
+  control.autoRotateSpeed = 0.5
+
+//add plane 
+
+const plainGeo = new THREE.PlaneGeometry(10,10);
+const planeMat = new THREE.MeshStandardMaterial({color:0xfffff})
+const planeMesh = new THREE.Mesh(plainGeo,planeMat)
+planeMesh.rotation.x=- Math.PI / 2
+planeMesh.receiveShadow = true;
+scene.add(planeMesh);
+
+
 
   let loader = new GLTFLoader();
   let mixer;
@@ -40,9 +74,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let anim = true;
 
   loader.load(
-    './public/models/characterModel.glb',
+    "/models/characterModel.glb",
     (gltf) => {
       model = gltf.scene;
+      model.castShadow = true;
       scene.add(model);
 
       if (gltf.animations.length > 0) {
@@ -52,58 +87,52 @@ document.addEventListener('DOMContentLoaded', () => {
         action.play();
 
         const animButton = document.querySelector(".animation-button");
-        animButton.addEventListener('click', () => {
-          
+        animButton.addEventListener("click", () => {
           if (anim) {
-            console.log("Pause animation");
+            control.autoRotate = false
             action.paused = true;
-            animButton.innerHTML = `start animation`
+            animButton.innerHTML = `start animation`;
           } else {
-            console.log("Resume animation");
-            animButton.innerHTML = `stop animation`
+            control.autoRotate = true
+            animButton.innerHTML = `stop animation`;
             action.paused = false;
           }
           anim = !anim;
         });
       }
     },
-    (xhr) => {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    (error) => {
-      console.error('An error happened while loading the model:', error);
-    }
   );
 
 
+  loader.load(
+    "/models/trees.glb",
+    (gltf) => {
+      model = gltf.scene;
+      scene.add(model);
+
+});
+
   // Orbit Controls
-  const control = new OrbitControls(camera, renderer.domElement);
-  control.enableDamping = true;
-  control.update();
 
 
+  const axesHelper = new THREE.AxesHelper( 5 );
+  scene.add( axesHelper );
 
-  window.addEventListener('resize', () => {
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    camera.aspect = container.clientWidth / container.clientHeight;
-    camera.updateProjectionMatrix();
-  });
+
 
   const clock = new THREE.Clock();
 
-
-
   function animate() {
+    
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    control.update();
+    control.update()
 
     const delta = clock.getDelta();
     if (mixer) mixer.update(delta);
-
   }
-
   animate();
-})
+  
+});
 
 
